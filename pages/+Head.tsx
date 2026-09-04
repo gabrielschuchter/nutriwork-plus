1f3fbebf78970efbe8c6f946cd7e5f31c31e1b2b
gabrielschuchter <schuchtergabriel@gmail.com>
2026-09-04 08:20:25 -0300

    fix: update Clarity project ID

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -18,7 +18,7 @@ const clarityScript = `
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "yci5qs3mr9");
+  })(window, document, "clarity", "script", "ycnddgjgxj");
 `;
 
 export function Head() {
